fb0b2bffcff17821ab5dea4eb46011723111fb41
Jadda Jaswanth Kumar <57146277+Jaswanth431@users.noreply.github.com>
2020-05-07 15:22:12 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,6 @@
 <?php
+    //database connection
     $conn = mysqli_connect('localhost', 'root', '', 'student_cell');
-    // $conn = mysqli_connect("148.66.137.19", "studentcell","Studentcell@123","student_cell");
+    
 ?>
 
